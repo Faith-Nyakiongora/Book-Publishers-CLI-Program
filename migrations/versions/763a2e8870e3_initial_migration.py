f100c0733041cad4dae97fa2b83ab5fabf6bbfab
+ <+>
2023-10-05 21:15:33 +0300

    Alembic

---
+++ migrations/versions/763a2e8870e3_initial_migration.py
@@ -0,0 +1,24 @@
+"""initial migration
+
+Revision ID: 763a2e8870e3
+Revises: 
+Create Date: 2023-10-05 20:51:34.394995
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "763a2e8870e3"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
